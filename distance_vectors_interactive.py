1b9642ec11862d61eb45ccb8a85f50258d1dcc04
batemanb <batemanb@berea.edu>
2021-06-16 09:58:30 -0500

    added tkinter interface to pick a file

--- distance_vectors_interactive.py
+++ distance_vectors_interactive.py
@@ -1,8 +1,12 @@
 import pandas
 import readmetaData
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
 
 # create a dataframe from an excel file
-df = pandas.read_excel('C:/Users/batemanb/Documents/Mizzou_UGR/SEM images/20210607/20210607.xlsx')
+Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+df = pandas.read_excel(filename)
 image1 = int(input("Enter number for first image"))
 image2 = int(input("Enter number for second image"))
 print(readmetaData.find_differences_pandas(df, image1, image2))
